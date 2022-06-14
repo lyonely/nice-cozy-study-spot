ee21be5c50dc87f64eac1b0d7fff039d0a8fe47b
yelun <ye.yang20@imperial.ac.uk>
2022-06-14 13:07:45 +0100

    feat: search bar implementation

---
+++ components/SearchBar.tsx
@@ -0,0 +1,15 @@
+import { TextInput } from '@mantine/core'
+import { Search } from 'react-feather';
+
+export default function SearchBar({ setTerm }) {
+
+	return (
+		<TextInput onChange={(event) => { setTerm(event.currentTarget.value) }}
+			type="search"
+			radius="md"
+			placeholder="Search Study Location"
+			icon={<Search size={14} />}
+			style={{ margin: "1.5em 0.5em" }}
+		/>
+	);
+}
--- pages/studyspaces.tsx
+++ pages/studyspaces.tsx
@@ -2,23 +2,13 @@ import React, { useState, useEffect } from 'react'
 import { fetcher } from "../utils/fetcher"
 import useSWR from 'swr'
 import StudySpaceList from '../components/studyspaceList'
-import TopBar from '../components/TopBar'
-import { MantineProvider } from '@mantine/core'
-import { AppTheme } from '../style/AppTheme'
 
 
 export default function Locations() {
-	const { data, error } = useSWR('/api/locations', fetcher)
-
-	useEffect(() => {
-		console.log(data)
-	}, [data])
 
 	return (<div className="bg">
 
-		{error ? "Error occured, please refresh the page" : data ?
-			<StudySpaceList studyspaces={data} />
-			: "Loading..."}
+		<StudySpaceList />
 	</div>
 	)
 }

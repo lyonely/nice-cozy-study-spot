1c7575284a2adac87e0925c43679f17e5b5627b1
Athena Yim <asy20@ic.ac.uk>
2022-06-14 14:59:30 +0100

    feat: capacity bar graph axes styling

--- components/CapacityGraph.tsx
+++ components/CapacityGraph.tsx
@@ -13,6 +13,14 @@ import { Bar } from 'react-chartjs-2'
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 
 export default function CapacityGraph() {
+    const outlineColor = () => {
+        return 'rgba(0, 0, 0, 255)'
+    }
+
+    const barColor = () => {
+        return 'rgba(175, 175, 175, 255)'
+    }
+
     const options = () => {
         return {
             responsive: true,
@@ -24,19 +32,51 @@ export default function CapacityGraph() {
                     display: false,
                 },
             },
+            scales: {
+                yAxis: {
+                    display: false,
+                },
+                xAxis: {
+                    display: true,
+                    grid: {
+                        drawTicks: false,
+                        drawOnChartArea: false,
+                        drawBorder: true,
+                        borderWidth: 1,
+                        borderColor: outlineColor(),
+                    },
+                    ticks: {
+                        maxTicksLimit: 6,
+                        maxRotation: 0,
+                        font: {
+                            family: "'DM Sans'",
+                            size: 16,
+                        },
+                        callback: function (value, index, ticks) {
+                            return `${this.getLabelForValue(value)}:00`
+                        },
+                    },
+                },
+            },
         }
     }
 
-    const labels = [1, 2, 3, 4]
+    const labels = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
 
     const data = () => {
         return {
             labels,
             datasets: [
                 {
-                    label: 'Dataset 1',
-                    data: labels,
-                    backgroundColor: 'rgba(100, 100, 100, 100)',
+                    label: 'Capacity',
+                    data: labels.map((i) => {
+                        return (i % 4) + 1
+                    }),
+                    //borderSkipped: 'false',
+                    backgroundColor: barColor(),
+                    borderColor: outlineColor(),
+                    borderWidth: 2,
+                    borderRadius: Number.MAX_VALUE,
                 },
             ],
         }

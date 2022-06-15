b0da2130da04f0f43f064ca12693d01fe36ab7eb
yelun <ye.yang20@imperial.ac.uk>
2022-06-15 10:16:19 +0100

    feat: button styling for sublocations improved

--- components/LocationCard.tsx
+++ components/LocationCard.tsx
@@ -1,11 +1,11 @@
 import {
-    Card,
-    Image,
-    Text,
-    Group,
-    Badge,
-    Accordion,
-    createStyles,
+	Card,
+	Image,
+	Text,
+	Group,
+	Badge,
+	Accordion,
+	createStyles,
 } from '@mantine/core'
 import { useState } from 'react'
 import { AppTheme } from '../style/AppTheme'
@@ -14,54 +14,54 @@ import Link from 'next/link'
 import CapacityBar from './CapacityBar'
 
 export default function LocationCard({ imgPath, name, sub_locations }) {
-    const useStyles = createStyles((theme) => ({
-        content: {
-            paddingLeft: 0,
-        },
-    }))
-    const { classes } = useStyles()
+	const useStyles = createStyles((theme) => ({
+		content: {
+			paddingLeft: 0,
+		},
+	}))
+	const { classes } = useStyles()
 
-    const min = Math.ceil(0)
-    const max = Math.floor(100)
-    const capacity = Math.floor(Math.random() * (max - min) + min)
+	const min = Math.ceil(0)
+	const max = Math.floor(100)
+	const capacity = Math.floor(Math.random() * (max - min) + min)
 
-    return (
-        <Card
-            shadow="sm"
-            radius="lg"
-            p="md"
-            withBorder
-            style={{
-                margin: '1.5em 0.5em 1.5em 0.5em',
-            }}
-        >
-            <Card.Section>
-                <Link href={name} passHref>
-                    <Image
-                        height={160}
-                        src={imgPath}
-                        alt={name}
-                        withPlaceholder
-                        placeholder={
-                            <Image
-                                height={160}
-                                src="https://www.imperial.ac.uk/media/migration/administration-and-support-services/library-3--tojpeg_1550242474436_x2.jpg"
-                            ></Image>
-                        }
-                    />
-                </Link>
-            </Card.Section>
-            <Link href={name} passHref>
-                <Group position="apart" style={{ marginTop: '1em' }}>
-                    <Text weight={500}>{name}</Text>
-                    <CapacityBar capacity={capacity} />
-                </Group>
-            </Link>
-            <Accordion classNames={classes}>
-                <Accordion.Item label="Details">
-                    <SubStudySpaces location={name} subareas={sub_locations} />
-                </Accordion.Item>
-            </Accordion>
-        </Card>
-    )
+	return (
+		<Card
+			shadow="sm"
+			radius="lg"
+			p="md"
+			withBorder
+			style={{
+				margin: '1.5em 0.5em 1.5em 0.5em',
+			}}
+		>
+			<Card.Section>
+				<Link href={name} passHref>
+					<Image
+						height={160}
+						src={imgPath}
+						alt={name}
+						withPlaceholder
+						placeholder={
+							<Image
+								height={160}
+								src="https://www.imperial.ac.uk/media/migration/administration-and-support-services/library-3--tojpeg_1550242474436_x2.jpg"
+							></Image>
+						}
+					/>
+				</Link>
+			</Card.Section>
+			<Link href={name} passHref>
+				<Group position="apart" style={{ marginTop: '1em' }}>
+					<Text weight={500}>{name}</Text>
+					<CapacityBar capacity={capacity} isSubLocation={false} />
+				</Group>
+			</Link>
+			<Accordion classNames={classes}>
+				<Accordion.Item label="Details">
+					<SubStudySpaces location={name} subareas={sub_locations} />
+				</Accordion.Item>
+			</Accordion>
+		</Card>
+	)
 }
--- components/substudyspaces.tsx
+++ components/substudyspaces.tsx
@@ -3,20 +3,26 @@ import { Badge, Group, Text, Button, Container, Center } from '@mantine/core'
 import CapacityBar from './CapacityBar'
 
 export default function SubStudySpaces({ location, subareas }) {
-    return subareas.map((subarea) => {
-        const min = Math.ceil(0)
-        const max = Math.floor(100)
-        const capacity = Math.floor(Math.random() * (max - min) + min)
+	return subareas.map((subarea) => {
+		const min = Math.ceil(0)
+		const max = Math.floor(100)
+		const capacity = Math.floor(Math.random() * (max - min) + min)
 
-        return (
-            <div className="sublocation-button">
-                <Link href={`/${location}/${subarea.name}`}>
-                    <Text align="left" style={{ width: '100%' }} weight={500}>
-                        {subarea.name}
-                    </Text>
-                </Link>
-                <CapacityBar capacity={capacity} />
-            </div>
-        )
-    })
+		return (
+			<Link href={`/${location}/${subarea.name}`}>
+				<Button classNames={{ inner: "sublocation__button", label: "sublocation__label" }} fullWidth variant="light" color="yellow" style={{ height: "auto", marginBottom: "0.25em", padding: "0.25em", color: "black" }}>
+					{/* <div className="sublocation-button"> */}
+					{/* <div style={{ width: "100%", display: "flex", flexDirection: "column" }}> */}
+					<Text align="center" style={{ width: '100%' }} weight={500}>
+						{subarea.name}
+					</Text>
+
+					<CapacityBar capacity={capacity} isSubLocation={true} />
+					{/* </div> */}
+					{/* </div> */}
+				</Button>
+
+			</Link>
+		)
+	})
 }

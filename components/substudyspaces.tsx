314616444213b9b4bd9803fc6cddfdf384b5a0e1
yelun <ye.yang20@imperial.ac.uk>
2022-06-15 15:32:40 +0100

    feat: change capacity functino

--- components/substudyspaces.tsx
+++ components/substudyspaces.tsx
@@ -1,40 +1,39 @@
 import Link from 'next/link'
 import { Text, Button } from '@mantine/core'
 import CapacityBar from './CapacityBar'
-import { capacity } from '../utils/capacity'
+import { subCapacity } from '../utils/capacity'
 
 export default function SubStudySpaces({ location, sub_locations }) {
-    return sub_locations.map((sub) => {
-        const curr = parseInt(sub.capacity)
-        const max = parseInt(sub.max_capacity)
+	return sub_locations.map((sub) => {
 
-        return (
-            <Link href={`/${location}/${sub.name}`}>
-                <Button
-                    classNames={{
-                        inner: 'sublocation__button',
-                        label: 'sublocation__label',
-                    }}
-                    fullWidth
-                    variant="light"
-                    color="yellow"
-                    style={{
-                        height: 'auto',
-                        marginBottom: '0.25em',
-                        padding: '0.25em',
-                        color: 'black',
-                    }}
-                >
-                    <Text align="center" style={{ width: '100%' }} weight={500}>
-                        {sub.name}
-                    </Text>
 
-                    <CapacityBar
-                        capacity={capacity(curr, max)}
-                        isSubLocation={true}
-                    />
-                </Button>
-            </Link>
-        )
-    })
+		return (
+			<Link href={`/${location}/${sub.name}`}>
+				<Button
+					classNames={{
+						inner: 'sublocation__button',
+						label: 'sublocation__label',
+					}}
+					fullWidth
+					variant="light"
+					color="yellow"
+					style={{
+						height: 'auto',
+						marginBottom: '0.25em',
+						padding: '0.25em',
+						color: 'black',
+					}}
+				>
+					<Text align="center" style={{ width: '100%' }} weight={500}>
+						{sub.name}
+					</Text>
+
+					<CapacityBar
+						capacity={subCapacity(sub)}
+						isSubLocation={true}
+					/>
+				</Button>
+			</Link>
+		)
+	})
 }
--- utils/capacity.ts
+++ utils/capacity.ts
@@ -1,1 +1,19 @@
-export const capacity = (cur, max) => max === 0 ? 0 : cur / max * 100
+const capacity = (curr, max) => max === 0 ? 0 : curr / max * 100
+
+export const subCapacity = (sub) => {
+	const curr = parseInt(sub.capacity)
+	const max = parseInt(sub.max_capacity)
+	return capacity(curr, max)
+}
+
+export const locationCapacity = (sub_locations) => {
+	const max = sub_locations.reduce(
+		(acc: number, { max_capacity }) => acc + parseInt(max_capacity),
+		0
+	)
+	const curr = sub_locations.reduce(
+		(acc, { capacity }) => acc + parseInt(capacity),
+		0
+	)
+	return capacity(curr, max)
+}

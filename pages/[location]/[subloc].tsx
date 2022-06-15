0c1e151d939c53f1836ca11c5af171ab1f2a2e02
Athena Yim <asy20@ic.ac.uk>
2022-06-15 17:04:10 +0100

    feat: wrap graph in container and add title

--- pages/[location]/[subloc].tsx
+++ pages/[location]/[subloc].tsx
@@ -2,8 +2,8 @@ import { useRouter } from 'next/router'
 import CapacityGraph from '../../components/CapacityGraph'
 import useSWR from 'swr'
 import { fetcher } from '../../utils/fetcher'
-import moment from 'moment'
 import { useEffect, useState } from 'react'
+import { Container } from '@mantine/core'
 
 export default function SubLocation() {
     const router = useRouter()
@@ -28,7 +28,12 @@ export default function SubLocation() {
             {data && sublocation ? (
                 <>
                     <p>{subloc}</p>
-                    <CapacityGraph datapoints={sublocation.trend_capacity} />
+                    <Container>
+                        <p>Daily capacity</p>
+                        <CapacityGraph
+                            datapoints={sublocation.trend_capacity}
+                        />
+                    </Container>
                 </>
             ) : (
                 <p>Loading...</p>

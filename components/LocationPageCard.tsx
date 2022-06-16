1cd9e1db0eb94cb1245d85587f6aa52606baa63a
Athena Yim <asy20@ic.ac.uk>
2022-06-16 16:31:17 +0100

    feat: fix margin, photo and fonts on location/sublocation cards

--- components/LocationPageCard.tsx
+++ components/LocationPageCard.tsx
@@ -8,112 +8,122 @@ import OpeningHoursAccordionItem from './OpeningHours'
 import moment from 'moment'
 
 export default function LocationPageCard({ location }) {
-	const { picture, name, sub_locations } = location
+    const { picture, name, sub_locations } = location
 
-	return (
-		<Card
-			shadow="sm"
-			radius="lg"
-			p="md"
-			withBorder
-			style={{
-				margin: '0.25em 0.5em 1.5em 0.5em',
-			}}
-		>
-			<Card.Section>
-				<Image
-					height={160}
-					src={picture}
-					alt={name}
-				/>
-			</Card.Section>
-			<Group
-				position="apart"
-				direction="column"
-				spacing="xs"
-				style={{ marginTop: '1em' }}
-			>
-				<Text weight={500}>{name}</Text>
-				<Text
-					size="sm"
-					color="gray"
-					style={{ paddingLeft: '0.5em' }}
-				>
-					capacity
-				</Text>
-				<CapacityBar
-					capacity={locationCapacity(sub_locations)}
-					isSubLocation={false}
-				/>
-			</Group>
-			<Accordion offsetIcon={false} initialItem={1}>
+    return (
+        <Card
+            shadow="sm"
+            radius="lg"
+            p="md"
+            withBorder
+            style={{
+                margin: '0.25em 0.5em 1.5em 0.5em',
+            }}
+        >
+            <Card.Section>
+                <Image height={160} src={picture} alt={name} />
+            </Card.Section>
+            <Group
+                position="apart"
+                direction="column"
+                spacing="xs"
+                style={{ marginTop: '1em' }}
+            >
+                <Text weight={600} size="xl">
+                    {name}
+                </Text>
+                <Text size="sm" color="gray" style={{ paddingLeft: '0.5em' }}>
+                    capacity
+                </Text>
+                <CapacityBar
+                    capacity={locationCapacity(sub_locations)}
+                    isSubLocation={false}
+                />
+            </Group>
+            <Accordion offsetIcon={false} initialItem={1}>
+                <Accordion.Item
+                    label={
+                        <AccordionLabel
+                            label={name}
+                            status={OpeningStatus({ location })}
+                        />
+                    }
+                >
+                    <OpeningHours location={location} />
+                </Accordion.Item>
 
-				<Accordion.Item label={<AccordionLabel label={name} status={OpeningStatus({ location })} />}>
-					<OpeningHours location={location} />
-				</Accordion.Item >
-
-				<Accordion.Item label="Capacities">
-					<SubStudySpaces
-						location={name}
-						sub_locations={sub_locations}
-					/>
-				</Accordion.Item>
-			</Accordion>
-		</Card >
-	)
+                <Accordion.Item label="Capacities">
+                    <SubStudySpaces
+                        location={name}
+                        sub_locations={sub_locations}
+                    />
+                </Accordion.Item>
+            </Accordion>
+        </Card>
+    )
 }
-
 
 // Functions for Opening Status of a location (not applicable to sub-locations)
 // Note: Mantine accordion can only have content of type Accordion.Item
 // So functions that actually return AccordionItem will not be listed
 interface AccordionLabelProps {
-	label: string;
-	status: string;
+    label: string
+    status: string
 }
 
 function AccordionLabel({ label, status }: AccordionLabelProps) {
-	let color;
-	switch (status) {
-		case "Open 24 Hours":
-		case "Open":
-			color = "green";
-			break;
-		case "Closed": color = "red"; break;
-		case "Closing Soon": color = "orange"; break;
-	}
-	return (
-		<div>
-			<Text>{label}</Text>
-			<Text size="sm" color={color} weight={400}>
-				{status}
-			</Text>
-		</div>
-	);
+    let color
+    switch (status) {
+        case 'Open 24 Hours':
+        case 'Open':
+            color = 'green'
+            break
+        case 'Closed':
+            color = 'red'
+            break
+        case 'Closing Soon':
+            color = 'orange'
+            break
+    }
+    return (
+        <div>
+            <Text>{label}</Text>
+            <Text size="sm" color={color} weight={400}>
+                {status}
+            </Text>
+        </div>
+    )
 }
 
 function OpeningStatus({ location }) {
-	const { time_open, time_closed } = location
+    const { time_open, time_closed } = location
 
-	// Displays "open", "closes" or "closes soon" (if location closes within an hour)
-	let openingTime = parseInt(moment(time_open).format('HH'));
-	let closingTime = parseInt(moment(time_closed).format('HH'));
+    // Displays "open", "closes" or "closes soon" (if location closes within an hour)
+    let openingTime = parseInt(moment(time_open).format('HH'))
+    let closingTime = parseInt(moment(time_closed).format('HH'))
 
-	var current = new Date();
-	var currentHour = current.getHours();
+    var current = new Date()
+    var currentHour = current.getHours()
 
-	let status;
+    let status
 
-	if (openingTime == closingTime) {
-		status = "Open 24 Hours"
-	} else if (currentHour < openingTime && currentHour + 24 >= closingTime) {
-		status = "Closed";
-	} else if (currentHour > openingTime && closingTime > openingTime && currentHour >= closingTime) {
-		status = "Closed";
-	} else if (currentHour == closingTime - 1 || closingTime == 0 && currentHour == 23) {
-		status = "Closing Soon"
-	} else {
-		status = "Open"
-	}
-	return status;
+    if (openingTime == closingTime) {
+        status = 'Open 24 Hours'
+    } else if (currentHour < openingTime && currentHour + 24 >= closingTime) {
+        status = 'Closed'
+    } else if (
+        currentHour > openingTime &&
+        closingTime > openingTime &&
+        currentHour >= closingTime
+    ) {
+        status = 'Closed'
+    } else if (
+        currentHour == closingTime - 1 ||
+        (closingTime == 0 && currentHour == 23)
+    ) {
+        status = 'Closing Soon'
+    } else {
+        status = 'Open'
+    }
+    return status
 }

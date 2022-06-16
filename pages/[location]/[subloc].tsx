f33e4b7a7a277d9f3abcc43bf378f4f19ecc5c3f
wansze <leewansze27@gmail.com>
2022-06-16 15:49:21 +0100

    chore: fixing rebase issue, readded cardaccess alert

--- pages/[location]/[subloc].tsx
+++ pages/[location]/[subloc].tsx
@@ -3,9 +3,9 @@ import CapacityGraph from '../../components/CapacityGraph'
 import useSWR from 'swr'
 import { fetcher } from '../../utils/fetcher'
 import { useEffect, useState } from 'react'
-import { Accordion, Card, Text, Divider, Group, List, ThemeIcon, Title } from '@mantine/core'
+import { Accordion, Card, Text, Divider, Group, List, ThemeIcon, Title, Alert } from '@mantine/core'
 import CapacityTag from '../../components/CapacityTag'
-import { Armchair, CircleCheck, CircleOff, Container, FileDescription, Pencil } from 'tabler-icons-react';
+import { AlertCircle, Armchair, CircleCheck, CircleOff, Container, FileDescription, Pencil } from 'tabler-icons-react';
 import BackButton from '../../components/BackButton'
 
 export default function SubLocation() {
@@ -21,81 +21,92 @@ export default function SubLocation() {
 
 
     return (
-        <Container>
-            <div>
-                <BackButton url={`/${location}`} text={location} />
-                <Card
-                    shadow="sm"
-                    radius="lg"
-                    p="md"
-                    withBorder
-                    style={{
-                        margin: '0.25em 0.5em 1.5em 0.5em',
-                    }}
-                >
-                    {data ? (
-                        <>
-                            <Group
-                                position="apart"
-                                direction="column"
-                                spacing="xs"
-                                style={{ marginTop: '0em' }}
-                            >
-                                <Title order={1}>{subloc}</Title>
 
-                                <Group>
-                                    <Text weight={500}> Current Capacity: </Text>
-                                    <CapacityTag
-                                        current={data.capacity}
-                                        max={data.max_capacity}
-                                    />
-                                </Group>
-                            </Group>
-                            <Group style={{ marginTop: '1em' }}>
-                                <Text weight={500}>Daily Capacity Trends</Text>
-                                <CapacityGraph
-                                    datapoints={data.trend_capacity}
+        <div>
+            <BackButton url={`/${location}`} text={location} />
+            <Card
+                shadow="sm"
+                radius="lg"
+                p="md"
+                withBorder
+                style={{
+                    margin: '0.25em 0.5em 1.5em 0.5em',
+                }}
+            >
+                {data ? (
+                    <>
+                        <Group
+                            position="apart"
+                            direction="column"
+                            spacing="xs"
+                            style={{ marginTop: '0em' }}
+                        >
+                            <Title order={1}>{subloc}</Title>
+
+                            <Group>
+                                <Text weight={500}> Current Capacity: </Text>
+                                <CapacityTag
+                                    current={data.capacity}
+                                    max={data.max_capacity}
                                 />
-
-
                             </Group>
-                            <Accordion offsetIcon={false}>
-                                <Accordion.Item
-                                    label="Description"
-                                    icon={
-                                        <ThemeIcon color="blue" variant="light" radius="xl">
-                                            <FileDescription size={20} />
-                                        </ThemeIcon>}
-                                >
-                                    <Text>{data.description}</Text>
-                                </Accordion.Item>
-                                <Accordion.Item
-                                    label="Amenities"
-                                    icon={
-                                        <ThemeIcon color="yellow" variant="light" radius="xl">
-                                            <Pencil size={20} />
-                                        </ThemeIcon>}
-                                >
-                                    {subLocAmenities(data)}
-                                </Accordion.Item>
-                                <Accordion.Item
-                                    label="Types of Study Spaces"
-                                    icon={
-                                        <ThemeIcon color="brown" variant="light" radius="xl">
-                                            <Armchair size={20} />
-                                        </ThemeIcon>}
-                                >
-                                    {subLocStudySpaces(data)}
-                                </Accordion.Item>
-                            </Accordion>
-                        </>
-                    ) : (
-                        <p>Loading...</p>
-                    )}
-                </Card>
-            </div>
-        </Container>
+                        </Group>
+                        <Group style={{ marginTop: '1em' }}>
+                            <Text weight={500}>Daily Capacity Trends</Text>
+                            <CapacityGraph
+                                datapoints={data.trend_capacity}
+                            />
+
+                            {cardAccessNeeded(data.card_access_needed)}
+                        </Group>
+                        <Accordion offsetIcon={false}>
+                            <Accordion.Item
+                                label="Description"
+                                icon={
+                                    <ThemeIcon color="blue" variant="light" radius="xl">
+                                        <FileDescription size={20} />
+                                    </ThemeIcon>}
+                            >
+                                <Text>{data.description}</Text>
+                            </Accordion.Item>
+                            <Accordion.Item
+                                label="Amenities"
+                                icon={
+                                    <ThemeIcon color="yellow" variant="light" radius="xl">
+                                        <Pencil size={20} />
+                                    </ThemeIcon>}
+                            >
+                                {subLocAmenities(data)}
+                            </Accordion.Item>
+                            <Accordion.Item
+                                label="Types of Study Spaces"
+                                icon={
+                                    <ThemeIcon color="beige" variant="light" radius="xl">
+                                        <Armchair size={20} />
+                                    </ThemeIcon>}
+                            >
+                                {subLocStudySpaces(data)}
+                            </Accordion.Item>
+                        </Accordion>
+                    </>
+                ) : (
+                    <p>Loading...</p>
+                )}
+            </Card>
+        </div>
+
     )
+}
+
+function cardAccessNeeded(cardAccess) {
+    if (cardAccess) {
+        return (
+            <Alert mt="md" mb="md" icon={<AlertCircle size={16} />} title="Card Access Required" color="red">
+                A staff or departmental card is needed to access this space.
+            </Alert>
+        );
+
+    }
 }
 
 

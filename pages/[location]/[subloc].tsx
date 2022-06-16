6be0aed9578c61f1be76fa06470313f4375b0622
wansze <leewansze27@gmail.com>
2022-06-16 14:26:15 +0100

    feat: added amenities list for sublocations

--- pages/[location]/[subloc].tsx
+++ pages/[location]/[subloc].tsx
@@ -3,9 +3,10 @@ import CapacityGraph from '../../components/CapacityGraph'
 import useSWR from 'swr'
 import { fetcher } from '../../utils/fetcher'
 import { useEffect, useState } from 'react'
-import { Accordion, Card, Container, Text, Group } from '@mantine/core'
+import { Accordion, Card, Text, Divider, Group, List, ThemeIcon, Title } from '@mantine/core'
 import CapacityBar from '../../components/CapacityBar'
 import CapacityTag from '../../components/CapacityTag'
+import { CircleCheck, CircleDashed, CircleOff, FileDescription, Pencil, PointOff, Space } from 'tabler-icons-react';
 
 export default function SubLocation() {
     const router = useRouter()
@@ -17,6 +18,7 @@ export default function SubLocation() {
     useEffect(() => {
         console.log(data)
     }, [data])
+
 
     return (
         <div>
@@ -38,9 +40,10 @@ export default function SubLocation() {
                                 spacing="xs"
                                 style={{ marginTop: '0em' }}
                             >
-                                <Text weight={500}>{subloc}</Text>
+                                <Title order={1}>{subloc}</Title>
+
                                 <Group>
-                                    <Text weight={500}> Capacity </Text>
+                                    <Text weight={500}> Current Capacity: </Text>
                                     <CapacityTag
                                         current={data.capacity}
                                         max={data.max_capacity}
@@ -48,14 +51,31 @@ export default function SubLocation() {
                                 </Group>
                             </Group>
                             <Group style={{ marginTop: '1em' }}>
-                                <Text weight={500}>Daily capacity</Text>
+                                <Text weight={500}>Daily Capacity Trends</Text>
                                 <CapacityGraph
                                     datapoints={data.trend_capacity}
                                 />
+
+
                             </Group>
                             <Accordion offsetIcon={false}>
-                                <Accordion.Item label="Description">
+                                <Accordion.Item
+                                    label="Description"
+                                    icon={
+                                        <ThemeIcon color="blue" variant="light" radius="xl">
+                                            <FileDescription size={20} />
+                                        </ThemeIcon>}
+                                >
                                     <Text>{data.description}</Text>
+                                </Accordion.Item>
+                                <Accordion.Item
+                                    label="Amenities"
+                                    icon={
+                                        <ThemeIcon color="yellow" variant="light" radius="xl">
+                                            <Pencil size={20} />
+                                        </ThemeIcon>}
+                                >
+                                    {subLocationAmenities(data)}
                                 </Accordion.Item>
                             </Accordion>
                         </>
@@ -66,4 +86,45 @@ export default function SubLocation() {
             </div>
         </div>
     )
+}
+
+
+function subLocationAmenities(sublocation) {
+
+    // Extract amenities into map
+    const { toilets_nearby, microwave, monitor, plug_sockets, printer, whiteboard, height_adjustable_desks } = sublocation;
+
+    return (
+        <div>
+            <Divider my="sm" />
+            <List
+                spacing="xs"
+                size="sm"
+                center
+            >
+                <List.Item icon={listIcon(toilets_nearby)}>Toilets Nearby</List.Item>
+                <List.Item icon={listIcon(microwave)}>Microwave Nearby</List.Item>
+                <List.Item icon={listIcon(monitor)}>Monitor</List.Item>
+                <List.Item icon={listIcon(plug_sockets)}>Plug Sockets</List.Item>
+                <List.Item icon={listIcon(printer)}>Printer</List.Item>
+                <List.Item icon={listIcon(whiteboard)}>WhiteBoard</List.Item>
+                <List.Item icon={listIcon(height_adjustable_desks)}>Height Adjustable Desks</List.Item>
+
+            </List>
+        </div>
+    )
+}
+
+function listIcon(available) {
+    if (available) {
+        return (
+            <ThemeIcon color="teal" size={24} radius="xl">
+                <CircleCheck size={16} />
+            </ThemeIcon>
+        );
+    } else {
+        return (<ThemeIcon color="red" size={24} radius="xl">
+            <CircleOff size={16} />
+        </ThemeIcon>);
+    }
 }

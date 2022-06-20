fd4746deab1e1397451f1261ab67df173e4f2bfc
yelun <ye.yang20@imperial.ac.uk>
2022-06-20 14:08:26 +0100

    chore: add logging for authenticaion base api

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -6,52 +6,53 @@ import { AppTheme } from '../style/AppTheme'
 import { useUser } from '@auth0/nextjs-auth0'
 
 export default function TopBar() {
-    const { height, width } = useViewportSize()
-    const { user: user } = useUser()
-    console.log(user)
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <div
-                style={{
-                    width: 500,
-                    // Media query with value from theme
-                    [`@media (maxWidth: ${AppTheme.breakpoints.xs}px)`]: {
-                        width: width,
-                    },
-                }}
-            >
-                <Group
-                    px="xs"
-                    position="apart"
-                    sx={(theme) => ({
-                        padding: '0.75em 0 0.75em 0',
-                        backgroundColor: theme.colors.brown[0],
-                    })}
-                >
-                    <Link href="/">
-                        <Group style={{ marginLeft: '0em' }}>
-                            <MapPin />
+	console.log(process.env.AUTH0_BASE_URL)
+	const { height, width } = useViewportSize()
+	const { user: user } = useUser()
+	console.log(user)
+	return (
+		<div style={{ display: 'flex', justifyContent: 'center' }}>
+			<div
+				style={{
+					width: 500,
+					// Media query with value from theme
+					[`@media (maxWidth: ${AppTheme.breakpoints.xs}px)`]: {
+						width: width,
+					},
+				}}
+			>
+				<Group
+					px="xs"
+					position="apart"
+					sx={(theme) => ({
+						padding: '0.75em 0 0.75em 0',
+						backgroundColor: theme.colors.brown[0],
+					})}
+				>
+					<Link href="/">
+						<Group style={{ marginLeft: '0em' }}>
+							<MapPin />
 
-                            <div className="hotbar__title">
-                                Study Space Finder
-                            </div>
-                        </Group>
-                    </Link>
-                    {user ? (
-                        <Link href="/api/auth/logout" passHref>
-                            <Button variant="subtle" compact={true}>
-                                <Text weight={300}>Log Out</Text>
-                            </Button>
-                        </Link>
-                    ) : (
-                        <Link href="/api/auth/login" passHref>
-                            <Button variant="subtle" compact={true}>
-                                <Text weight={300}>Log In</Text>
-                            </Button>
-                        </Link>
-                    )}
-                </Group>
-            </div>
-        </div>
-    )
+							<div className="hotbar__title">
+								Study Space Finder
+							</div>
+						</Group>
+					</Link>
+					{user ? (
+						<Link href="/api/auth/logout" passHref>
+							<Button variant="subtle" compact={true}>
+								<Text weight={300}>Log Out</Text>
+							</Button>
+						</Link>
+					) : (
+						<Link href="/api/auth/login" passHref>
+							<Button variant="subtle" compact={true}>
+								<Text weight={300}>Log In</Text>
+							</Button>
+						</Link>
+					)}
+				</Group>
+			</div>
+		</div>
+	)
 }

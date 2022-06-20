746b6da1ec6d1157f81854313ee26b45ddb57a1b
yelun <ye.yang20@imperial.ac.uk>
2022-06-20 13:56:08 +0100

    fix: auth fix

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -1,3 +1,19 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin, handleProfile } from "@auth0/nextjs-auth0";
 
-export default handleAuth()
+export default handleAuth({
+	async login(req, res) {
+		try {
+			await handleLogin(req, res, {
+				returnTo: '/api/auth/me',
+				authorizationParams: {
+					response_type: 'code',
+					scope: 'openid profile email',
+				}
+			});
+
+		} catch (error) {
+			res.status(error.status || 400).end(error.message);
+		}
+
+	}
+})

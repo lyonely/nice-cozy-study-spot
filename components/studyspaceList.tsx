24fb16b47de11f7fdce354adb0c5a1241dae9cb9
Athena Yim <asy20@ic.ac.uk>
2022-06-21 14:31:08 +0100

    feat: sort and filter menu UI

--- components/studyspaceList.tsx
+++ components/studyspaceList.tsx
@@ -1,4 +1,4 @@
-import { Container, Text } from '@mantine/core'
+import { Container, Group, MultiSelect, Select, Text } from '@mantine/core'
 import LocationListCard from './LocationCard'
 import SearchBar from './SearchBar'
 import { useState, useEffect } from 'react'
@@ -6,11 +6,39 @@ import SubLocationSearchCard from './SubLocationSearchCard'
 import LoadingCircle from './LoadingCircle'
 import { useUser } from '@auth0/nextjs-auth0'
 
+const allSortOrders = () => {
+    return [
+        { value: 'a-z', label: 'A-Z' },
+        { value: 'z-a', label: 'Z-A' },
+        { value: 'capacity-incr', label: 'Capacity (low-high)' },
+        { value: 'capacity-decr', label: 'Capacity (high-low)' },
+    ]
+}
+
+const allFilters = () => {
+    return [
+        { value: 'card_access_needed', label: 'Card access' },
+        { value: 'microwave', label: 'Microwave' },
+        { value: 'printer', label: 'Printer' },
+        { value: 'monitor', label: 'Monitor' },
+        { value: 'plug_sockets', label: 'Charging sockets' },
+        { value: 'whiteboard', label: 'Whiteboard' },
+        { value: 'toilets_nearby', label: 'Toilets nearby' },
+        { value: 'quiet_study_available', label: 'Quiet study' },
+        { value: 'group_study_available', label: 'Group study' },
+        { value: 'silent_study_available', label: 'Silent study' },
+        { value: 'breakout_space_available', label: 'Breakout spaces' },
+        { value: 'height_adjustable_desks', label: 'Height adjustable desks' },
+    ]
+}
+
 export default function StudySpaceList() {
     const [term, setTerm] = useState('')
     const [data, setData] = useState<any[]>()
     const [subLocData, setSubLocData] = useState<any[]>()
     const { user } = useUser()
+    const [sortOrder, setSortOrder] = useState('')
+    const [filters, setFilters] = useState<any[]>()
 
     const fetchLocations = async () => {
         const resp = await fetch(`/api/locations?term=${term}`)
@@ -40,6 +68,28 @@ export default function StudySpaceList() {
     return data ? (
         <Container>
             <SearchBar term={term} setTerm={setTerm} />
+            <Group
+                direction="row"
+                position="apart"
+                spacing="xs"
+                style={{ margin: '0 0.5em 0.5em 0.5em' }}
+            >
+                <Select
+                    placeholder="Sort by:"
+                    value={sortOrder}
+                    onChange={setSortOrder}
+                    data={allSortOrders()}
+                    style={{ maxWidth: '40%' }}
+                />
+                <MultiSelect
+                    placeholder="Filter by:"
+                    data={allFilters().sort((x, y) => {
+                        return x.label.localeCompare(y.label)
+                    })}
+                    clearable
+                    style={{ maxWidth: '60%' }}
+                />
+            </Group>
             {data && data.length !== 0 && (
                 <>
                     <Text size="xs" align="center" color="gray">

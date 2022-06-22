037ce6df1a81eabe3d30051c5da1a5bc9e560ed2
Athena Yim <asy20@ic.ac.uk>
2022-06-22 12:55:51 +0100

    feat: add search term and sort order to app context

--- utils/state.tsx
+++ utils/state.tsx
@@ -1,14 +1,21 @@
 import { createContext, useContext, useState } from 'react'
 
 const AppContext = createContext({
-    filters: [],
-    setFilters: (x) => {},
+    filter: { filters: [], setFilters: (x) => {} },
+    search: { term: '', setTerm: (x) => {} },
+    sort: { sortOrder: '', setSortOrder: (x) => {} },
 })
 
 export function AppWrapper({ children }) {
     const [filters, setFilters] = useState([])
+    const [term, setTerm] = useState('')
+    const [sortOrder, setSortOrder] = useState('')
 
-    const value = { filters, setFilters }
+    const value = {
+        filter: { filters, setFilters },
+        search: { term, setTerm },
+        sort: { sortOrder, setSortOrder },
+    }
 
     return <AppContext.Provider value={value}>{children}</AppContext.Provider>
 }

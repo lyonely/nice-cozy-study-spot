ad8b3ba27ab1e317dde6789b6f8db20b8744594e
yelun <ye.yang20@imperial.ac.uk>
2022-06-22 15:46:31 +0100

    fix: rebase error fix

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,60 +3,35 @@ import { useRef } from 'react'
 import { Search, X } from 'react-feather'
 
 export default function SearchBar({ term, setTerm }) {
-    const searchRef = useRef<HTMLInputElement>()
+	const searchRef = useRef<HTMLInputElement>()
 
-    const ClearSearch = ({ value }) => {
-        console.log('SEARCH')
-        console.log(value)
-        return (
-            <X
-                strokeWidth="xs"
-                color="gray"
-                size="18px"
-                onClick={() => {
-                    searchRef.current.value = null
-                    setTerm([])
-                }}
-            />
-        )
-    }
-
-<<<<<<< HEAD
-    return (
-        <TextInput
-            ref={searchRef}
-            onChange={(event) => {
-                setTerm(event.currentTarget.value)
-            }}
-            type="search"
-            radius="md"
-            placeholder="Search Study Location"
-            icon={<Search size={14} />}
-            rightSection={term ? <ClearSearch value={this} /> : <></>}
-            value={term}
-            style={{ margin: '0.75em 0.5em 0.5em 0.5em' }}
-        />
-    )
-=======
 	const ClearSearch = ({ value }) => {
-		return (<X strokeWidth='xs' color="gray" size="18px" onClick={
-			() => {
-				searchRef.current.value = null
-				setTerm([])
-			}} />)
+		return (
+			<X
+				strokeWidth="xs"
+				color="gray"
+				size="18px"
+				onClick={() => {
+					searchRef.current.value = null
+					setTerm([])
+				}}
+			/>
+		)
 	}
 
 	return (
-		<TextInput ref={searchRef} onChange={(event) => {
-			setTerm(event.currentTarget.value)
-		}}
+		<TextInput
+			ref={searchRef}
+			onChange={(event) => {
+				setTerm(event.currentTarget.value)
+			}}
 			type="search"
 			radius="md"
 			placeholder="Search Study Location"
 			icon={<Search size={14} />}
 			rightSection={term ? <ClearSearch value={this} /> : <></>}
-			style={{ margin: "0.75em 0.5em 0.5em 0.5em" }}
+			value={term}
+			style={{ margin: '0.75em 0.5em 0.5em 0.5em' }}
 		/>
-	);
->>>>>>> feat: favourite tab in studylocation list
+	)
 }

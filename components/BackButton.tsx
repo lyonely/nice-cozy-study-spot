587ccf2b0880afb85a6a8bd557b836e3d5329252
yelun <ye.yang20@imperial.ac.uk>
2022-06-23 14:18:07 +0100

    feat: report button relocated and back button function change

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -7,11 +7,11 @@ import { ArrowLeft } from "react-feather";
 export default function BackButton({ url, text }) {
 	const router = useRouter()
 	return (
-		<Link href={url}>
-			<Group spacing="xs" style={{ padding: "0.5em 0 0.5em 0.5em" }}>
-				<ArrowLeft strokeWidth="xs" />
-				<Text weight={250}>Back</Text>
-			</Group>
-		</Link>
+		// <Link href={url}>
+		<Group onClick={() => router.back()} spacing="xs" style={{ padding: "0.5em 0 0.5em 0.5em" }}>
+			<ArrowLeft strokeWidth="xs" />
+			<Text weight={250}>Back</Text>
+		</Group>
+		// </Link>
 	)
 }
